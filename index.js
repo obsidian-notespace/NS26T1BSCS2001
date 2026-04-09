05b4e03cca018ed6fbcdcba7022a1e1bab04c709
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-04-09 07:54:57 +0530

    source: https://seek.onlinedegree.iitm.ac.in/courses/ns_26t1_cs2001?id=142

---
+++ index.js
@@ -0,0 +1,41 @@
+window.htmlFiles = [
+    {
+        "path": "sol/sessions/WEEK 05/L5.1 - Relational Database Design1/L5.1 - Relational Database Design 1.html"
+    },
+    {
+        "path": "sol/sessions/WEEK 05/L5.1 - Relational Database Design1/P01.html"
+    },
+    {
+        "path": "sol/sessions/WEEK 05/L5.2 - Relational Database Design 2/L5.2 - Relational Database Design 2.html"
+    },
+    {
+        "path": "sol/sessions/WEEK 05/L5.3 - Relational Database Design 3/L5.3 - Relational Database Design 3.html"
+    },
+    {
+        "path": "sol/sessions/WEEK 05/L5.4 - Relational Database Design 4/L5.4 - Relational Database Design 4.html"
+    },
+    {
+        "path": "sol/sessions/WEEK 05/L5.5 - Relational Database Design 5/L5.5 - Relational Database Design 5.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ5.1_ Activity Questions 1 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ5.2_ Activity Questions 2 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ5.3_ Activity Questions 3 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ5.4_ Activity Questions 4 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ5.5_ Activity Questions 5 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/Graded Assignment 5 __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/Practice assignment 5 - Not Graded __ IITM Online Degree.html"
+    }
+];
